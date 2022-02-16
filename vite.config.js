6b9bd516d95bf8db993603c32228f184582269cd
Shun Git <kwokshungit@gmail.com>
2022-02-16 23:13:44 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/rubiks-cubicle',
+  build: {
+    assetsDir: '.',
+    cssCodeSplit: false,
+  },
   plugins: [
     vue(),
-  ]
+  ],
 })
